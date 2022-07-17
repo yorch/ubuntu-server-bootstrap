51a29bcb57d352e907585dd69d4dcd0140d32662
Jorge Barnaby <jorge.barnaby@gmail.com>
2022-07-17 17:00:41 -0400

    Fix

--- run-tests.sh
+++ run-tests.sh
@@ -13,7 +13,7 @@ for base_image in "${BASE_IMAGES[@]}"; do
     docker build \
         --progress=plain \
         --build-arg BASE_IMAGE=${base_image} \
-        -t ${IMAGE_TAG_BASE}-${BASE_IMAGE} \
+        -t "${IMAGE_TAG_BASE}-${base_image}" \
         -f ${DOCKERFILE} \
         .
 done
