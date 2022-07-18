317fb2ad85ff15b3ba51fe371129363ca960ae73
Jorge Barnaby <jorge.barnaby@gmail.com>
2022-07-17 20:37:19 -0400

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,14 +15,13 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 TIMEZONE=America/New_York
 LOCALES=(
     "en_US.UTF-8"
-    "en_CA.UTF-8"
 )
 
 APT_CMD="apt-get -qq" # -qq includes -y
 APT_UPDATE="${APT_CMD} update"
 APT_INSTALL="${APT_CMD} install"
 APT_AUTOREMOVE="${APT_CMD} autoremove"
-APT_LOG_FILE="apt-operations.log"
+LOG_FILE="bootstrap_$(date +'%Y%m%d%H%M%S').log"
 USR_BIN_DIR=/usr/local/bin
 # -s, --silent        Silent mode
 # -S, --show-error    Show error even when -s is used
@@ -55,22 +54,24 @@ function downloadLatestRelease {
 }
 
 # Update all current packages
-${APT_UPDATE} && ${APT_CMD} upgrade && ${APT_AUTOREMOVE} &>> ${APT_LOG_FILE}
+${APT_UPDATE} && ${APT_CMD} upgrade && ${APT_AUTOREMOVE} &>> ${LOG_FILE}
 
 # Timezone
-TIMEDATECTL=timedatectl
-if command -v "${TIMEDATECTL}"; then
-  ${TIMEDATECTL} set-timezone ${TIMEZONE}
+echo "Setting timezone to ${TIMEZONE}..."
+if [ -x "$(command -v timedatectl)" ]; then
+  timedatectl set-timezone ${TIMEZONE} &>> ${LOG_FILE}
 fi
 
 # Locales
+echo "Setting locales to ${LOCALES[*]}..."
 LOCALE_GEN=locale-gen
-if ! command -v "${LOCALE_GEN}"; then
-  ${APT_INSTALL} locales &>> ${APT_LOG_FILE}
+if ! [ -x "$(command -v ${LOCALE_GEN})" ]; then
+  ${APT_INSTALL} locales &>> ${LOG_FILE}
 fi
-locale-gen ${LOCALES[@]}
+${LOCALE_GEN} ${LOCALES[@]} &>> ${LOG_FILE}
 
 # Tools
+echo "Installing tools..."
 ${APT_INSTALL} \
     byobu \
     curl \
@@ -80,7 +81,7 @@ ${APT_INSTALL} \
     tig \
     vim \
     wget \
-    &>> ${APT_LOG_FILE}
+    &>> ${LOG_FILE}
 
 # Install latest Docker version
 echo "Installing Docker..."
@@ -89,22 +90,22 @@ ${APT_INSTALL} \
     ca-certificates \
     gnupg-agent \
     software-properties-common \
-    &>> ${APT_LOG_FILE}
-${CURL_CMD} https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88 &>> ${APT_LOG_FILE}
+    &>> ${LOG_FILE}
+${CURL_CMD} https://download.docker.com/linux/ubuntu/gpg | apt-key add - \&>> ${LOG_FILE}
+apt-key fingerprint 0EBFCD88 &>> ${LOG_FILE}
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" \
-   &>> ${APT_LOG_FILE}
+   &>> ${LOG_FILE}
 ${APT_UPDATE} && ${APT_INSTALL} \
     docker-ce \
     docker-ce-cli \
     containerd.io \
     docker-compose-plugin \
-    &>> ${APT_LOG_FILE}
+    &>> ${LOG_FILE}
 
-# Install Docker Compose
+# Docker Compose
 # https://github.com/docker/compose
 DOCKER_CLI_PLUGINS_DIR="/usr/local/lib/docker/cli-plugins"
 DOCKER_COMPOSE_BIN="${DOCKER_CLI_PLUGINS_DIR}/docker-compose"
@@ -119,7 +120,7 @@ else
     echo "Docker Compose already installed."
 fi
 
-# Install Docker Compose Switch (to ease transition from Docker Compose v1)
+# Docker Compose Switch (to ease transition from Docker Compose v1)
 DOCKER_COMPOSE_SWITCH_BIN="${USR_BIN_DIR}/compose-switch"
 DOCKER_COMPOSE_SWITCH_REPO="docker/compose-switch"
 DOCKER_COMPOSE_SWITCH_ASSET="docker-compose-linux-amd64"
@@ -127,21 +128,21 @@ if ! [ -e ${DOCKER_COMPOSE_SWITCH_BIN} ]; then
     echo "Installing Docker Switch..."
     downloadLatestRelease "${DOCKER_COMPOSE_SWITCH_REPO}" "${DOCKER_COMPOSE_SWITCH_ASSET}" "${DOCKER_COMPOSE_SWITCH_BIN}"
     # Set Docker Compose Switch to replace Docker Compose v1
-    update-alternatives --install ${USR_BIN_DIR}/docker-compose docker-compose "${DOCKER_COMPOSE_SWITCH_BIN}" 99
+    update-alternatives --install ${USR_BIN_DIR}/docker-compose docker-compose "${DOCKER_COMPOSE_SWITCH_BIN}" 99 &>> ${LOG_FILE}
     echo "Docker Switch installed."
 else
     echo "Docker Switch already installed."
 fi
 
-# Install NeoVim
+# NeoVim
 # Adds repo for latest neovim version
-add-apt-repository -y ppa:neovim-ppa/stable
-${APT_UPDATE} && ${APT_INSTALL} neovim &>> ${APT_LOG_FILE}
+add-apt-repository -y ppa:neovim-ppa/stable &>> ${LOG_FILE}
+${APT_UPDATE} && ${APT_INSTALL} neovim &>> ${LOG_FILE}
 # Set neovim as default vim
-update-alternatives --set vi $(which nvim)
-update-alternatives --set vim $(which nvim)
+update-alternatives --set vi $(which nvim) &>> ${LOG_FILE}
+update-alternatives --set vim $(which nvim) &>> ${LOG_FILE}
 
-# Install SpeedTest
+# SpeedTest
 # https://github.com/sivel/speedtest-cli
 SPEEDTEST_BIN="${USR_BIN_DIR}/speedtest-cli"
 SPEEDTEST_REPO="sivel/speedtest-cli"
@@ -164,10 +165,10 @@ if ! [ -x "$(command -v python)" ] || ! [ -e ${PYTHON_BIN} ]; then
     fi
 fi
 
-# Install ZSH and Prezto
+# ZSH and Prezto
 # https://github.com/sorin-ionescu/prezto
 echo "Installing ZSH and Prezto..."
-${APT_INSTALL} zsh &>> ${APT_LOG_FILE}
+${APT_INSTALL} zsh &>> ${LOG_FILE}
 ZSH_BIN=$(command -v zsh)
 PREZTO_DIR="${HOME}/.zprezto"
 PREZTORC_URL="https://raw.githubusercontent.com/yorch/ubuntu-server-bootstrap/main/.zpreztorc"
@@ -175,7 +176,7 @@ PREZTO_REPO_URL="https://github.com/sorin-ionescu/prezto.git"
 
 if [ -x "${ZSH_BIN}" ]; then
     if ! [ -d "${PREZTO_DIR}" ]; then
-        git clone --recursive "${PREZTO_REPO_URL}" "${PREZTO_DIR}"
+        git clone --recursive "${PREZTO_REPO_URL}" "${PREZTO_DIR}" &>> ${LOG_FILE}
         ${CURL_CMD} "${PREZTORC_URL}" -o "${PREZTO_DIR}/runcoms/zpreztorc"
         ${ZSH_BIN} -c "
             setopt EXTENDED_GLOB
@@ -191,18 +192,23 @@ else
     echo "ERROR - Could not find ZSH even though we tried to install it"
 fi
 
-# Install or update SpaceVim
+# SpaceVim
+echo "Installing or updating SpaceVim..."
+${APT_INSTALL} fontconfig &>> ${LOG_FILE}
 ${CURL_CMD} https://spacevim.org/install.sh | bash
 
 # Enable multiplexer `byobu`
 # byobu-enable
 
 # Cleanup old packages
-${APT_AUTOREMOVE} &>> ${APT_LOG_FILE}
+echo "Cleaning up old packages..."
+${APT_AUTOREMOVE} &>> ${LOG_FILE}
+
 # Cleanup caches
-${APT_CMD} clean &>> ${APT_LOG_FILE}
+echo "Cleanup caches..."
+${APT_CMD} clean &>> ${LOG_FILE}
 
 echo
 echo "All Done! You should restart the machine now!"
-echo "A log file is available at ${APT_LOG_FILE}"
+echo "A log file is available at ${LOG_FILE}"
 echo
