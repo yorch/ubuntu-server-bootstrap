fd92a03e970c692d9e847929ba0b5feec9e53d9b
Jorge Barnaby <jorge.barnaby@gmail.com>
2025-03-23 09:37:25 -0400

    Update CI to use GHA matrix for running tests (#11)

--- run-tests.sh
+++ run-tests.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 BASE_IMAGES=(
-    # "ubuntu:18.04"
     "ubuntu:20.04"
     "ubuntu:22.04"
     "ubuntu:24.04"
 )
+
+BASE_IMAGE=${1:-${BASE_IMAGES[0]}}
+if [[ ! " ${BASE_IMAGES[@]} " =~ " ${BASE_IMAGE} " ]]; then
+    echo "Invalid base image. Choose from: ${BASE_IMAGES[*]}"
+    exit 1
+fi
+
 IMAGE_TAG_BASE=ubuntu-server-bootstrap
 
-for base_image in "${BASE_IMAGES[@]}"; do
-    echo "Running tests with ${base_image}"
-    docker build \
-        --progress=plain \
-        --build-arg BASE_IMAGE=${base_image} \
-        -t "${IMAGE_TAG_BASE}-${base_image}" \
-        .
-done
+echo "Running tests with ${BASE_IMAGE}"
+
+docker build \
+    --progress=plain \
+    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    -t "${IMAGE_TAG_BASE}-${BASE_IMAGE}" \
+    .
